2e8bf8748aa91bcf65821197fe74487d56a0059a
Codeless-Coded <yashmittal05sxcs@gmail.com>
2024-04-16 03:29:56 +0530

    Noting

--- lib/Messages/messageBackend.dart
+++ lib/Messages/messageBackend.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 
-class messageDB {
+class  messageDB {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
